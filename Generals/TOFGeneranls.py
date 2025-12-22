c110b1ba03d2d019e21b97ee95c3d4954b949df6
linyuejie <linyuejie@szbl.ac.cn>
2025-12-22 19:07:07 +0800

    renew MLEM in current style

--- Generals/TOFGeneranls.py
+++ Generals/TOFGeneranls.py
@@ -16,4 +16,4 @@ class TOFOption:
         self.light_speed = 299792458  # unit: m/s
         self.tof_bin_width_in_ps = tof_bin_width_in_ps
         self.tof_bin_width_in_mm = tof_bin_width_in_ps * 1e-12 * self.light_speed * 1e3
-        self.sigma_tof = np.asarray([(tof_resolution * 1e-12 * self.light_speed * 1e3) / (2 * np.sqrt(2*np.log(2)))], dtype=np.float32)
+        self.sigma_tof = np.asarray([(tof_resolution * 1e-12 * self.light_speed * 1e3 / 2) / (2 * np.sqrt(2*np.log(2)))], dtype=np.float32)
--- Methods/MLEM.py
+++ Methods/MLEM.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 from temp.functions_from_kernal_MLAA import trl_curvature
 
 
-class MLAA(ReconOption):
+class MLEM(ReconOption):
     def __init__(self, img_dim: np.array, voxel_size: np.array, output_dir: str, scanner_option: ScannerOption, psf_option: PointSpreadFunction, tof_option: TOFOption, ac_map: np.array, mu_map: np.array, ex_cdf_path=None, device_id=0):
         super().__init__(img_dim=img_dim, voxel_size=voxel_size, ex_cdf_path=ex_cdf_path, output_dir=output_dir, scanner_option=scanner_option, psf_option=psf_option, num_of_iterations=0, num_of_subsets=0, device_id=0, tx_cdf_path=None, bx_cdf_path=None)
         self.projector = Projector(self, scanner_option, psf_option, tof_option, device_id)
@@ -34,6 +34,7 @@ class MLAA(ReconOption):
                 counts=ai,
                 add_psf=False
             )
+        self.sense_img.astype(np.float32).tofile(self.output_dir + "/sense_img.raw")
 
     def set_0_outsize_fov(self, img):
         h, w = img.shape[:2]  # 获取图像高度和宽度（忽略通道数）
@@ -55,8 +56,7 @@ class MLAA(ReconOption):
     def get_ac_map_update(self):
         ai = self.projector.projection_forward_lors(self.mu_map, self.measurement[:, 1], self.measurement[:, 2], False)
         ai = np.repeat(ai[:, np.newaxis], self.tof_option.tof_bin_num, axis=1)
-        bi = self.projector.projection_forward_lors_wtof(self.ac_map, self.measurement[:, 1], self.measurement[:, 2], False)
-        # bi = ai * bi
+        bi = ai * self.projector.projection_forward_lors_wtof(self.ac_map, self.measurement[:, 1], self.measurement[:, 2], False)
         ratio_yi_bi = (self.yi / bi)
 
         np.nan_to_num(ratio_yi_bi, copy=False, nan=0, posinf=0, neginf=0)
@@ -66,44 +66,10 @@ class MLAA(ReconOption):
             sinogram=ratio_yi_bi * ai,
             add_psf=False
         )
-        self.get_sense_img()
         self.ac_map = self.ac_map / self.sense_img * ac_update
         self.ac_map[self.sense_img == 0] = 0
         np.nan_to_num(self.ac_map, copy=False, nan=0, posinf=0, neginf=0)
         self.ac_map = self.set_0_outsize_fov(self.ac_map)
-
-    def get_mu_map_update(self):
-        bi = self.projector.projection_forward_lors_wtof(self.ac_map, self.measurement[:, 1], self.measurement[:, 2], False)
-        ai = np.exp(-self.projector.projection_forward_lors_wtof(self.mu_map, self.measurement[:, 1], self.measurement[:, 2], False))
-        bp_aibi = self.projector.projection_backward_wtof(self.measurement[:, 1], self.measurement[:, 2], (ai * bi), False)
-        bp_yi = self.projector.projection_backward_wtof(self.measurement[:, 1], self.measurement[:, 2], self.yi, False)
-        mu_update = (self.alpha_p / self.n) * (1 - bp_yi / bp_aibi)
-        np.nan_to_num(mu_update, copy=False, nan=0, posinf=0, neginf=0)
-        self.mu_map += mu_update
-        self.mu_map = self.set_0_outsize_fov(self.mu_map)
-
-    def get_attn_ml_sps(self):
-        # from kernel MLAA
-        bi = self.projector.projection_forward_lors(self.ac_map, self.measurement[:, 0], self.measurement[:, 1],
-                                                    False)
-        ai = np.exp(
-            -self.projector.projection_forward_lors(self.mu_map, self.measurement[:, 0], self.measurement[:, 1],
-                                                    False))
-        yb = ai * bi
-        yt = self.measurement[:, 2]
-        yr = (1 - yt / yb) * yb
-        gx = self.projector.projection_backward(np.column_stack((
-            self.measurement[:, 0], self.measurement[:, 1], yr
-        )), False)
-        nt = trl_curvature(yt, bi, 0, bi, 'oc')
-        aa = self.projector.projection_forward_lors(np.ones_like(self.ac_map), self.measurement[:, 0],
-                                                    self.measurement[:, 1], False)
-        wx = self.projector.projection_backward(np.column_stack((
-            self.measurement[:, 0], self.measurement[:, 1], nt * aa
-        )), False)
-
-        self.mumap = self.mumap + gx / wx
-        self.mu_map[wx == 0] = 0
 
     def get_objection_function_score(self):
         ai = self.projector.projection_forward_lors_wtof(self.mu_map, self.measurement[:, 1], self.measurement[:, 2], False)
@@ -123,14 +89,8 @@ class MLAA(ReconOption):
         obj_func_score = np.zeros(iteration)
         for i in range(iteration):
             self.get_ac_map_update()
-            self.get_mu_map_update()
             self.ac_map.astype(np.float32).tofile(self.output_dir + r"\ac_map_it%d.raw" % i)
-            self.mu_map.astype(np.float32).tofile(self.output_dir + r"\mu_map_it%d.raw" % i)
             obj_func_score[i] = self.get_objection_function_score()
-            self.sense_img.astype(np.float32).tofile(self.output_dir + "/sense_img_it%d.raw" % i)
-            # plt.plot(obj_func_score)
-            # plt.scatter(np.arange(iteration), obj_func_score, s=30, marker="*")
-            # plt.savefig(r"D:\BaiduNetdiskDownload\data_for_mlaa\test_output\tof_mlaa\score.jpg", bbox_inches="tight")
             obj_func_score.astype(np.float32).tofile(self.output_dir + r"\score.raw")
 
 
@@ -138,13 +98,13 @@ if __name__ == "__main__":
     os.chdir(r"D:\linyuejie\git-project\pet-reconstruction-in-github")
     scanner_option = ScannerOption("WBBrain_20251219")
     ac_map = np.ones([170, 170, 170])
-    mu_map = np.flip(np.fromfile(r"D:\linyuejie\BaiduSyncdisk\data_for_mlaa\initial_mumap_dim170.raw", dtype=np.float32).reshape(170, 170, 170).transpose([2, 1, 0]), axis=1)
+    mu_map = np.flip(np.fromfile(r"D:\linyuejie\BaiduSyncdisk\data_for_mlaa\clear_mumap_dim170.raw", dtype=np.float32).reshape(170, 170, 170).transpose([2, 1, 0]), axis=1)
     psf_option = PointSpreadFunction(sigma=1)
     tof_option = TOFOption(tof_resolution=300, tof_bin_num=21, tof_bin_width_in_ps=100)
-    mlaa = MLAA(
+    mlem = MLEM(
         img_dim=np.array([170, 170, 170]),
         voxel_size=np.array([1, 1, 1]),
-        output_dir=r"D:\linyuejie\BaiduSyncdisk\data_for_mlaa\test_output\tof_mlaa",
+        output_dir=r"D:\linyuejie\BaiduSyncdisk\data_for_mlaa\test_output\tof_mlem",
         scanner_option=scanner_option,
         ex_cdf_path=r"D:\linyuejie\BaiduSyncdisk\data_for_mlaa\trues.cdf",
         psf_option=psf_option,
@@ -153,4 +113,4 @@ if __name__ == "__main__":
         mu_map=mu_map,
         tof_option=tof_option
     )
-    mlaa.run()
+    mlem.run()
