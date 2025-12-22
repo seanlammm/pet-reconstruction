fix bug in tof index calculation

Generals/ReconGenerals.py
@@ -104,19 +104,16 @@ class ReconOption:
 
         coins = np.frombuffer(data, dtype=current_dtype)
         coins = np.column_stack((coins["castor_id_1"], coins["castor_id_2"], coins["tof"]))
-
         # 这里为了避免后续有重复的 id 相反的 LOR，决定把小 id 放在 id1 ，大 id 放 id2
         # 在后续读取 tof 时要注意将 tof 置反
         flip_flag = coins[:, 0] > coins[:, 1]
-        coins[flip_flag, 0], coins[flip_flag, 1], coins[flip_flag, 2] = coins[flip_flag, 1].copy(), coins[flip_flag, 0].copy(), -coins[flip_flag, 2]
-
+        coins[flip_flag, 0], coins[flip_flag, 1], coins[flip_flag, 2] = coins[flip_flag, 1].copy(), coins[flip_flag, 0].copy(), -coins[flip_flag, 2].copy()
         # TOF bin 从 -max(tof) 到 max(tof)，tof_bin_num 需要单数
-        tof_range = [-tof_option.tof_bin_num * tof_option.tof_bin_width_in_ps / 2, tof_option.tof_bin_num * tof_option.tof_bin_width_in_ps / 2]
-        tof_interval = np.linspace(tof_range[0], tof_range[1], tof_option.tof_bin_num + 1)
 
         # out_of_range = (coins[:, 2] < tof_range[0]) | (coins[:, 2] >= tof_range[1])
         # coins = coins[~out_of_range, :]
-        tof_bin_index = np.digitize(coins[:, 2], bins=tof_interval) - 1
+        tof_pos = coins[:, 2] * tof_option.light_speed / 2
+        tof_bin_index = np.digitize(tof_pos, tof_option.tof_bin_edges) - 1
         tof_bin_index[tof_bin_index < 0] = 0
         tof_bin_index[tof_bin_index >= tof_option.tof_bin_num] = tof_option.tof_bin_num - 1
 
@@ -124,12 +121,12 @@ class ReconOption:
         uni_coins, uni_row_index = np.unique(coins_1d, return_inverse=True)
         coins_id_1 = uni_coins // self.scanner_option.crystal_num
         coins_id_2 = uni_coins % self.scanner_option.crystal_num
-        coins = np.column_stack((coins_id_1, coins_id_2))
-        histo = np.zeros([coins.shape[0], self.tof_option.tof_bin_num], dtype=int)
+        uni_coins = np.column_stack((coins_id_1, coins_id_2))
+        histo = np.zeros([uni_coins.shape[0], tof_option.tof_bin_num], dtype=int)
         np.add.at(histo, tuple(np.column_stack((uni_row_index, tof_bin_index)).astype(int).T), 1)
 
         # coins = [id_1, id2], histo = [n_events, tof_bin]
-        return coins.astype(int), histo
+        return uni_coins.astype(int), histo
 
 
 

Generals/TOFGeneranls.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class TOFOption:
-    def __init__(self, tof_resolution, tof_bin_num, tof_bin_width_in_ps, nsigmas=3, tof_center_offset=0):
+    def __init__(self, tof_resolution, tof_bin_num, tof_range_in_ps, nsigmas=3, tof_center_offset=0):
         '''
         Args:
             tof_resolution: unit: ps
@@ -13,7 +13,8 @@ class TOFOption:
         self.tof_bin_num = tof_bin_num
         self.nsigmas = nsigmas
         self.tof_center_offset = np.asarray([tof_center_offset], dtype=np.float32)
-        self.light_speed = 299792458  # unit: m/s
-        self.tof_bin_width_in_ps = tof_bin_width_in_ps
-        self.tof_bin_width_in_mm = tof_bin_width_in_ps * 1e-12 * self.light_speed * 1e3
-        self.sigma_tof = np.asarray([(tof_resolution * 1e-12 * self.light_speed * 1e3 / 2) / (2 * np.sqrt(2*np.log(2)))], dtype=np.float32)
+        self.light_speed = 299792458 * 1e3 / 1e12  # unit: mm/ps
+        self.tof_range_in_mm = tof_range_in_ps * self.light_speed
+        self.tof_bin_edges = np.linspace(-1, 1, tof_bin_num+1) * self.tof_range_in_mm / 2
+        self.tof_bin_width = self.tof_bin_edges[1] - self.tof_bin_edges[0]
+        self.sigma_tof = np.asarray([(tof_resolution * self.light_speed / 2) / (2 * np.sqrt(2*np.log(2)))], dtype=np.float32)

Methods/MLEM.py
@@ -53,7 +53,7 @@ class MLEM(ReconOption):
         return result
 
     def get_ac_map_update(self):
-        bi = self.projector.projection_forward_lors_wtof(self.ac_map, self.events_LOR[:, 0],self.events_LOR[:, 1], False)
+        bi = self.projector.projection_forward_lors_wtof(self.ac_map, self.events_LOR[:, 0], self.events_LOR[:, 1], False)
         ratio_yi_bi = (self.yi / bi)
 
         np.nan_to_num(ratio_yi_bi, copy=False, nan=0, posinf=0, neginf=0)
@@ -80,28 +80,29 @@ class MLEM(ReconOption):
         iteration = 5
         self.events_LOR, self.yi = self.get_coins_wtof(self.ex_cdf_path, tof_option, 0)
         # self.get_sense_img()
-        self.sense_img = np.fromfile(r"D:\BaiduSyncdisk\data_for_mlaa\test_output\tof_mlem_ac\sense_img.raw", dtype=np.float32).reshape([170, 170, 170])
+        self.sense_img = np.fromfile(self.output_dir + r"\sense_img.raw", dtype=np.float32).reshape([170, 170, 170])
+        # self.sense_img = np.fromfile(r"C:\Users\ct-guys\Downloads\sens_SmartBraintPET_atten_170.bin", dtype=np.float32).reshape([170, 170, 170]).transpose([2,1,0])
         obj_func_score = np.zeros(iteration)
         for i in range(iteration):
             self.get_ac_map_update()
             self.ac_map.astype(np.float32).tofile(self.output_dir + r"\ac_map_it%d.raw" % i)
             obj_func_score[i] = self.get_objection_function_score()
-            obj_func_score.astype(np.float32).tofile(self.output_dir + r"\score.raw")
+        obj_func_score.astype(np.float32).tofile(self.output_dir + r"\score.raw")
 
 
 if __name__ == "__main__":
-    os.chdir(r"D:\github_code\pet-reconstruction")
+    os.chdir(r"D:\linyuejie\git-project\pet-reconstruction-in-github")
     scanner_option = ScannerOption("WBBrain_20251219")
     ac_map = np.ones([170, 170, 170])
-    mu_map = np.flip(np.fromfile(r"D:\BaiduSyncdisk\data_for_mlaa\clear_mumap_dim170.raw", dtype=np.float32).reshape(170, 170, 170).transpose([2, 1, 0]), axis=1)
+    mu_map = np.flip(np.fromfile(r"D:\linyuejie\BaiduSyncdisk\data_for_mlaa\clear_mumap_dim170.raw", dtype=np.float32).reshape(170, 170, 170).transpose([2, 1, 0]), axis=1)
     psf_option = PointSpreadFunction(sigma=1)
-    tof_option = TOFOption(tof_resolution=300, tof_bin_num=21, tof_bin_width_in_ps=100)
+    tof_option = TOFOption(tof_resolution=300, tof_bin_num=21, tof_range_in_ps=1000)
     mlem = MLEM(
         img_dim=np.array([170, 170, 170]),
         voxel_size=np.array([1, 1, 1]),
-        output_dir=r"D:\BaiduSyncdisk\data_for_mlaa\test_output\tof_mlem",
+        output_dir=r"D:\linyuejie\BaiduSyncdisk\data_for_mlaa\test_output\tof_mlem",
         scanner_option=scanner_option,
-        ex_cdf_path=r"D:\BaiduSyncdisk\data_for_mlaa\trues.cdf",
+        ex_cdf_path=r"D:\linyuejie\BaiduSyncdisk\data_for_mlaa\trues.cdf",
         psf_option=psf_option,
         device_id=0,
         ac_map=ac_map,