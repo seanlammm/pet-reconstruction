6ac9b3b090dd309154522952f6bd83844f7e4f30
seanlmmm <sseeaannlam@gmail.com>
2025-12-28 23:31:04 +0800

    update in MLEM
    1. full lor projection

--- Methods/MLEM.py
+++ Methods/MLEM.py
@@ -24,17 +24,14 @@ class MLEM(ReconOption):
         self.n = 170
 
     def get_sense_img(self):
-        self.sense_img = np.zeros(self.img_dim)
-        for i in tqdm(range(self.scanner_option.crystal_per_layer), desc="calculating sensesitivity image..."):
-            ai = self.projector.projection_forward_lors(self.mu_map, np.ones([self.scanner_option.crystal_per_layer - i]) * i, np.arange(i, self.scanner_option.crystal_per_layer), False)
-            ai = np.exp(-ai)
-            self.sense_img += self.projector.projection_backward(
-                start_index=np.ones([self.scanner_option.crystal_per_layer - i]) * i,
-                end_index=np.arange(i, self.scanner_option.crystal_per_layer),
-                counts=ai,
-                add_psf=False
-            )
-        self.sense_img.astype(np.float32).tofile(self.output_dir + "/sense_img.raw")
+        ai = self.projector.projection_forward_lors(self.mu_map, self.events_LOR[:, 0], self.events_LOR[:, 1], False)
+        ai = np.exp(-ai)
+        self.sense_img = self.projector.projection_backward(
+            start_index=self.events_LOR[:, 0],
+            end_index=self.events_LOR[:, 1],
+            counts=ai,
+            add_psf=False
+        )
 
     def set_0_outsize_fov(self, img):
         h, w = img.shape[:2]  # 获取图像高度和宽度（忽略通道数）
@@ -70,41 +67,37 @@ class MLEM(ReconOption):
         self.ac_map = self.set_0_outsize_fov(self.ac_map)
 
     def get_objection_function_score(self):
-        ai = np.exp(-self.projector.projection_forward_lors(self.mu_map, self.events_LOR[:, 0], self.events_LOR[:, 1], False))
-        ai = np.repeat(ai[:, np.newaxis], self.tof_option.tof_bin_num, axis=1)
         bi = self.projector.projection_forward_lors_wtof(self.ac_map, self.events_LOR[:, 0], self.events_LOR[:, 1], False)
-        ri = ai * bi
-        flag = ri > 0
-        score = np.sum(-ri[flag] + self.yi[flag] * np.log(ri[flag]))
+        flag = bi > 0
+        score = np.sum(-bi[flag] + self.yi[flag] * np.log(bi[flag]))
         return score
 
     def run(self):
-        iteration = 5
-        self.events_LOR, self.yi = self.get_coins_wtof(self.ex_cdf_path, tof_option, 0)
+        iteration = 20
+        self.events_LOR, self.yi = self.get_coins_wtof(self.ex_cdf_path, tof_option, start_index=0, scanner_option=scanner_option, return_with_full_lor=True)
         self.get_sense_img()
-        # self.sense_img = np.fromfile(self.output_dir + r"\sense_img.raw", dtype=np.float32).reshape([170, 170, 170])
-        # self.sense_img = np.fromfile(r"C:\Users\ct-guys\Downloads\sens_SmartBraintPET_atten_170.bin", dtype=np.float32).reshape([170, 170, 170]).transpose([2,1,0])
         obj_func_score = np.zeros(iteration)
         for i in range(iteration):
             self.get_ac_map_update()
             self.ac_map.astype(np.float32).tofile(self.output_dir + r"\ac_map_it%d.raw" % i)
             obj_func_score[i] = self.get_objection_function_score()
         obj_func_score.astype(np.float32).tofile(self.output_dir + r"\score.raw")
+        self.sense_img.astype(np.float32).tofile(self.output_dir + "/sense_img.raw")
 
 
 if __name__ == "__main__":
-    os.chdir(r"D:\linyuejie\git-project\pet-reconstruction-in-github")
+    os.chdir(r"D:\github_code\pet-reconstruction")
     scanner_option = ScannerOption("WBBrain_20251219")
     ac_map = np.ones([170, 170, 170]).transpose([2, 1, 0])
-    mu_map = np.fromfile(r"D:\linyuejie\BaiduSyncdisk\data_for_mlaa\clear_mumap_dim170.raw", dtype=np.float32).reshape(170, 170, 170).transpose([2, 1, 0])
+    mu_map = np.fromfile(r"D:\BaiduSyncdisk\data_for_mlaa\clear_mumap_dim170.raw", dtype=np.float32).reshape(170, 170, 170).transpose([2, 1, 0])
     psf_option = PointSpreadFunction(sigma=1)
     tof_option = TOFOption(tof_resolution=300, tof_bin_num=21, tof_range_in_ps=1000)
     mlem = MLEM(
         img_dim=np.array([170, 170, 170]),
         voxel_size=np.array([1, 1, 1]),
-        output_dir=r"D:\linyuejie\BaiduSyncdisk\data_for_mlaa\test_output\tof_mlem",
+        output_dir=r"D:\BaiduSyncdisk\data_for_mlaa\test_output\tof_mlem",
         scanner_option=scanner_option,
-        ex_cdf_path=r"D:\linyuejie\BaiduSyncdisk\data_for_mlaa\trues.cdf",
+        ex_cdf_path=r"D:\BaiduSyncdisk\data_for_mlaa\10millions_sim\trues.cdf",
         psf_option=psf_option,
         device_id=0,
         ac_map=ac_map,
